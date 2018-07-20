bff58d2d8effc0f0c32d4ca025a9069645d56415
Cadetho <cadetho@gmail.com>
2018-07-20 17:22:37 -0500

    First Commit

---
+++ wahaya.php
@@ -0,0 +1,12 @@
+<?php
+/*
+Template Name: Wahaya
+*/
+
+get_header(); ?>
+	<div class="content landing">
+		<div class="svgAnimWrapper">
+			<object type="image/svg+xml" data="<?php echo get_bloginfo('template_url') ?>/assets/images/wahaya-circle_union.svg">
+			</object>
+		</div>
+	</div>
